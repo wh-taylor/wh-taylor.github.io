697394f6cff249223eb8a3f0001e65e27ec23ca4
Hayden Taylor <h.taylor2106@gmail.com>
2025-04-14 15:14:19 -0400

    extract header

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,23 @@
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
-import logo from './images/logo.svg';
-import gitHubLogo from './images/githublogo.svg';
-import linkedInLogo from './images/linkedinlogo.svg';
-import name from './images/name.svg';
+import React, { JSX, useState } from 'react';
 import { Home } from './pages/Home'
+import { Header } from './Header';
 
 import './App.css';
 
 type Page = "Home" | "Research" | "Projects" | "Talks";
 
-function App() {
+export interface PageProps {
+  page: Page;
+  setPage: React.Dispatch<React.SetStateAction<Page>>;
+}
+
+function App(): JSX.Element {
   const [page, setPage] = useState<Page>("Home");
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className="App-header-div">
-          <a href=".">
-            <img src={logo} className="App-logo" alt="logo" />
-          </a>
-          <div className="App-header-text">
-            <a href=".">
-              <img src={name} className="App-logo-name" alt="Hayden Taylor" />
-            </a>
-            <p>Undergraduate Mathematician and Computer Scientist.</p>
-          </div>
-          <nav>
-            <Button 
-              className={page === "Home" ? "active" : "inactive"}
-              onClick={() => setPage("Home")}>
-              HOME
-            </Button>
-            <Button
-              className={page === "Research" ? "active" : "inactive"}
-              onClick={() => setPage("Research")}>
-              RESEARCH
-            </Button>
-            <Button
-              className={page === "Projects" ? "active" : "inactive"}
-              onClick={() => setPage("Projects")}>
-              PROJECTS
-            </Button>
-            <Button
-              className={page === "Talks" ? "active" : "inactive"}
-              onClick={() => setPage("Talks")}>
-              TALKS
-            </Button>
-          </nav>
-          <div className="App-header-links">
-            <a href="https://github.com/wh-taylor">
-              <img src={gitHubLogo} alt="GitHub Link" />
-            </a>
-            <a href="https://www.linkedin.com/in/william-hayden-taylor/">
-              <img src={linkedInLogo} alt="LinkedIn Link" />
-            </a>
-          </div>
-        </div>
+        <Header page={page} setPage={setPage} />
       </header>
       <div className="big-picture">
       </div>
---
+++ src/Header.tsx
@@ -0,0 +1,53 @@
+import { JSX } from 'react';
+import { Button } from 'react-bootstrap';
+import logo from './images/logo.svg';
+import gitHubLogo from './images/githublogo.svg';
+import linkedInLogo from './images/linkedinlogo.svg';
+import name from './images/name.svg';
+import { PageProps } from './App';
+
+export function Header({page, setPage}: PageProps): JSX.Element {
+    return (
+        <div className="App-header-div">
+          <a href=".">
+            <img src={logo} className="App-logo" alt="logo" />
+          </a>
+          <div className="App-header-text">
+            <a href=".">
+              <img src={name} className="App-logo-name" alt="Hayden Taylor" />
+            </a>
+            <p>Undergraduate Mathematician and Computer Scientist.</p>
+          </div>
+          <nav>
+            <Button 
+              className={page === "Home" ? "active" : "inactive"}
+              onClick={() => setPage("Home")}>
+              HOME
+            </Button>
+            <Button
+              className={page === "Research" ? "active" : "inactive"}
+              onClick={() => setPage("Research")}>
+              RESEARCH
+            </Button>
+            <Button
+              className={page === "Projects" ? "active" : "inactive"}
+              onClick={() => setPage("Projects")}>
+              PROJECTS
+            </Button>
+            <Button
+              className={page === "Talks" ? "active" : "inactive"}
+              onClick={() => setPage("Talks")}>
+              TALKS
+            </Button>
+          </nav>
+          <div className="App-header-links">
+            <a href="https://github.com/wh-taylor">
+              <img src={gitHubLogo} alt="GitHub Link" />
+            </a>
+            <a href="https://www.linkedin.com/in/william-hayden-taylor/">
+              <img src={linkedInLogo} alt="LinkedIn Link" />
+            </a>
+          </div>
+        </div>
+    );
+}
