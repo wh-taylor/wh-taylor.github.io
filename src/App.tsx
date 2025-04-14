b8f27c0d56f5b1e7e3558f426336f42f15236954
Hayden Taylor <h.taylor2106@gmail.com>
2025-04-14 15:05:39 -0400

    extract homepage

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ import logo from './images/logo.svg';
 import gitHubLogo from './images/githublogo.svg';
 import linkedInLogo from './images/linkedinlogo.svg';
 import name from './images/name.svg';
-import portrait from './images/portrait.jpg';
-import emailLogo from './images/emaillogo.svg';
-import institutionLogo from './images/institutionlogo.svg';
+import { Home } from './pages/Home'
+
 import './App.css';
 
 type Page = "Home" | "Research" | "Projects" | "Talks";
@@ -61,35 +60,18 @@ function App() {
       </header>
       <div className="big-picture">
       </div>
-      <div className="App-body">
-        <img className="portrait" src={portrait} alt="Portrait of Hayden Taylor" />
-        <h1>W. Hayden Taylor</h1>
-        <h2>University of Delaware</h2>
-        <h3>
-          Mathematics (H.B.S.) and Computer Science (B.S.)
-        </h3>
-        <div className="listing">
-          <img src={emailLogo} alt="Email" />
-          <a href="mailto:h.taylor2106@gmail.com">
-            h.taylor2106@gmail.com
-          </a>
-        </div>
-        <div className="listing">
-          <img src={institutionLogo} alt="Institution" />
-          <a href="mailto:haydent@udel.com">
-            haydent@udel.edu
-          </a>
-        </div>
-        <p>
-          I am an undergraduate student at the University of Delaware. I am driven to actively pursue research, create impactful projects, and educate.
-        </p>
-        <p>
-          My research interests are primarily in knot theory, machine learning, abstract algebra, data science, and graph theory.
-        </p>
-        <p>
-          For the spring of 2025, I am taking a teaching assistant role in CISC 181: Introduction to Computer Science II where I assist students in developing a strong foundation in TypeScript.
-        </p>
-      </div>
+      {
+        (() => {switch (page) {
+          case 'Home':
+            return <Home />;
+          case 'Research':
+            return <Home />;
+          case 'Projects':
+            return <Home />;
+          case 'Talks':
+            return <Home />;
+        }})()
+      }
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import portrait from '../images/portrait.jpg';
+import emailLogo from '../images/emaillogo.svg';
+import institutionLogo from '../images/institutionlogo.svg';
+
+export function Home() {
+    return (
+        <div className="App-body">
+            <img className="portrait" src={portrait} alt="Portrait of Hayden Taylor" />
+            <h1>W. Hayden Taylor</h1>
+            <h2>University of Delaware</h2>
+            <h3>
+            Mathematics (H.B.S.) and Computer Science (B.S.)
+            </h3>
+            <div className="listing">
+            <img src={emailLogo} alt="Email" />
+            <a href="mailto:h.taylor2106@gmail.com">
+                h.taylor2106@gmail.com
+            </a>
+            </div>
+            <div className="listing">
+            <img src={institutionLogo} alt="Institution" />
+            <a href="mailto:haydent@udel.com">
+                haydent@udel.edu
+            </a>
+            </div>
+            <p>
+            I am an undergraduate student at the University of Delaware. I am driven to actively pursue research, create impactful projects, and educate.
+            </p>
+            <p>
+            My research interests are primarily in knot theory, machine learning, abstract algebra, data science, and graph theory.
+            </p>
+            <p>
+            For the spring of 2025, I am taking a teaching assistant role in CISC 181: Introduction to Computer Science II where I assist students in developing a strong foundation in TypeScript.
+            </p>
+        </div>
+    );
+}
