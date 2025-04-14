6c8ac9e824cc000c3a9259a0f34865975cba5e84
Hayden Taylor <h.taylor2106@gmail.com>
2025-04-14 14:50:44 -0400

    add more to home description

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
             <a href=".">
               <img src={name} className="App-logo-name" alt="Hayden Taylor" />
             </a>
-            <p>Undergraduate Mathematician.</p>
+            <p>Undergraduate Mathematician and Computer Scientist.</p>
           </div>
           <nav>
             <Button 
@@ -64,7 +64,10 @@ function App() {
       <div className="App-body">
         <img className="portrait" src={portrait} alt="Portrait of Hayden Taylor" />
         <h1>W. Hayden Taylor</h1>
-        <p>Mathematics and computer science undergraduate student at the University of Delaware.</p>
+        <h2>University of Delaware</h2>
+        <h3>
+          Mathematics (H.B.S.) and Computer Science (B.S.)
+        </h3>
         <div className="listing">
           <img src={emailLogo} alt="Email" />
           <a href="mailto:h.taylor2106@gmail.com">
@@ -77,6 +80,15 @@ function App() {
             haydent@udel.edu
           </a>
         </div>
+        <p>
+          I am an undergraduate student at the University of Delaware. I am driven to actively pursue research, create impactful projects, and educate.
+        </p>
+        <p>
+          My research interests are primarily in knot theory, machine learning, abstract algebra, data science, and graph theory.
+        </p>
+        <p>
+          For the spring of 2025, I am taking a teaching assistant role in CISC 181: Introduction to Computer Science II where I assist students in developing a strong foundation in TypeScript.
+        </p>
       </div>
     </div>
   );
