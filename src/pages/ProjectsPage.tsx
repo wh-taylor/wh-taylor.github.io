8f54a4dbd970ea05fb9d7909588535768c4a5c8c
Hayden Taylor <h.taylor2106@gmail.com>
2025-04-17 00:01:27 -0400

    update problematic subtitle

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -18,7 +18,7 @@ export function ProjectsPage() {
                 index={1}
                 src={problematic}
                 title="Problematic"
-                subtitle="Written in React."
+                subtitle="Written in React with TypeScript."
                 text="An AI-powered problemset generation tool. Using Google Gemini's API, given a prompt, a PDF of generated problems along with an answer key is provided as output. The PDF is compiled via LaTeX, and the LaTeX code is viewable. Primarily intended for mathematics professors and students who want an efficient and effective way to generate new practice problems."
                 />
             <ProjectEntry
